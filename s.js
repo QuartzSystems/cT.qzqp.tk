a06948aa6405efcc25a6badcc29eb5e0e073cc7a
Ari S. <echomanari@gmail.com>
2015-05-29 07:38:41 -0700

    Update s.js

--- s.js
+++ s.js
@@ -7,7 +7,8 @@
         }
         var dat = data;
         for(var d in dat) {
-            $('#cardboard').append("<div class='col s12 m6'><div class='card-panel'><a href='" + dat[d].link + "'><h4>" + d + "</h4></a><br/><p>" + dat[d].desc + "</p></div></div>");
+            $("<div id='card" + d + "' class='col s12 m6'><div class='card-panel'><a href='" + dat[d].link + "'><h4>" + d + "</h4></a><br/><p>" + dat[d].desc + "</p></div></div>").css({scale:0}).appendTo('#cardboard');
+            $('#card' + d).transition({scale:1});
         }
     });
 })();
